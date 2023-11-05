94c3e65c059be220367fc4bcd8b032649e2f8a59
Phuc Dang <peaetchyousee@gmail.com>
2023-11-05 03:14:54 -0600

    checkpoint 2
    done with phase 1 of key exchange

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,20 @@
-use client::{generate_all_keys, publish_all_keys};
+use client::{generate_all_keys, publish_all_keys, kdf};
+use serde_json::json;
 
+
+// #[tokio::main]
 fn main() {
-    generate_all_keys(false);
-    publish_all_keys("s","s");
+    // generate_all_keys(false);
+    // let email = "asd@yahoo.com";
+    // let username = "asdasf";
+    // let password = "bobobobo";
+    // let js = json!({
+    //     "username": username,
+    //     "email": email,
+    //     "password": password,
+    // });
+    // println!("{}", publish_all_keys(&js.to_string(),"http://10.169.129.170:8080/register").await);
+    let b: [u8; 32] = [12; 32];
+    let h = kdf(&b);
+    println!("{:?}", h);
 }
